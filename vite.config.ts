56e7ece71b8be62e6ae95b027e5e172abf0c1249
Ryan Champion <ryan.champion2@icloud.com>
2026-01-18 07:35:49 -0500

    fuckyou

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,17 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), devtoolsJson()]
+	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+	build: {
+		sourcemap: false,
+		rollupOptions: {
+			onwarn(warning, defaultHandler) {
+				// Suppress Flowbite @__PURE__ sourcemap noise
+				if (warning.code === 'SOURCEMAP_ERROR' && /flowbite-svelte/.test(warning.loc?.file || '')) {
+					return;
+				}
+				defaultHandler(warning);
+			}
+		}
+	}
 });
